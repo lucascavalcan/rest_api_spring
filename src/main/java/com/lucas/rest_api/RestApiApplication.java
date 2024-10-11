a06d4cf5de62b5e3570e1431f4bb15b70990500e
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-10-11 15:16:46 -0300

    class User - Lombok

--- src/main/java/com/lucas/rest_api/RestApiApplication.java
+++ src/main/java/com/lucas/rest_api/RestApiApplication.java
@@ -1,5 +1,6 @@
 package com.lucas.rest_api;
 
+import com.lucas.rest_api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+
 	}
 
 }
--- src/main/java/com/lucas/rest_api/domain/User.java
+++ src/main/java/com/lucas/rest_api/domain/User.java
@@ -1,66 +1,24 @@
 package com.lucas.rest_api.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.Objects;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "TB_USER")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
-
-    public User() {
-    }
-
-    public User(String name) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
