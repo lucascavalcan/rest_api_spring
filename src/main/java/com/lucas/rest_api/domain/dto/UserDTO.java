aac415972aed75f9c90be541cd8f286e74be8cc7
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-10-11 17:25:41 -0300

    class UserDTO

--- src/main/java/com/lucas/rest_api/domain/dto/UserDTO.java
+++ src/main/java/com/lucas/rest_api/domain/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package com.lucas.rest_api.domain.dto;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
 }
