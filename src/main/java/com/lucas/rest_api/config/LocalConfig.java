cdabda45d126302539028b553a45abf20c324300
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-10-11 16:36:15 -0300

    Tests profile

---
+++ src/main/java/com/lucas/rest_api/config/LocalConfig.java
@@ -0,0 +1,27 @@
+package com.lucas.rest_api.config;
+
+import com.lucas.rest_api.domain.User;
+import com.lucas.rest_api.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+//tests profile configuration
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostConstruct
+    public void startDB() {
+        User u1 = new User(null, "Lucas", "lucas@gmail.com", "123");
+        User u2 = new User(null, "Albert Einstein", "albert@gmail.com", "123");
+
+        repository.saveAll(List.of(u1, u2));
+    }
+}
